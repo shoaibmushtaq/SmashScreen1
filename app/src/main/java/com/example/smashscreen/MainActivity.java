06c806c823bad60d473291679e2bc47350462631
Muhammad Shoaib <shoaibmushtaq2@gmail.com>
2019-05-06 15:58:30 +0500

    Added Second Screen on click listener.

--- app/src/main/java/com/example/smashscreen/MainActivity.java
+++ app/src/main/java/com/example/smashscreen/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.smashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(getApplicationContext(),SecondScreen.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
     }
 }
